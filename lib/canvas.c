997d48c190c0d776cece8044e11d2d3c9dd31399
stanley.mcdaniel <stanley.mcdaniel@servicenow.com>
2022-12-04 09:48:22 -0800

    Module 18 complete

--- lib/canvas.c
+++ lib/canvas.c
@@ -14,7 +14,20 @@ struct Circle {
     int cb; // color for blue
 };
 
+// Circle annimation struct
+struct CircleAnimationData {
+    int x;  // x coordinate
+    int y;  // y coordinate
+    int r;  // radias
+    int yv; // y - axis velocity
+    int xv; // x - axis velocity
+    int xd; // x - axis direction (1 = forward)
+    int yd; // y - axis direction (1 = forward)
+};
+
+
 struct Circle circles[NUM_CIRCLES];
+struct CircleAnimationData animationData[NUM_CIRCLES];
 
 int getRand(int max){
     return (rand() % max);
@@ -31,10 +44,21 @@ int main(){
     for(int i=0; i < NUM_CIRCLES; i++){
         // Radias
         int radias = getRand(50);
+        
         // Coordinates
         int x = getRand(1000)+radias;
         int y = getRand(1000)+radias; 
-        // Now set the values in the struct
+
+        // Fill animation data struct - i
+        animationData[i].x = x;
+        animationData[i].y = y;
+        animationData[i].r = radias;
+        animationData[i].xv = getRand(10);
+        animationData[i].yv = getRand(10);
+        animationData[i].xd = 1;
+        animationData[i].yd = 1;
+
+        // Now set the values in the struct - i
         circles[i].x = x;
         circles[i].y = y;
         circles[i].r = radias;
@@ -48,8 +72,45 @@ int main(){
     return 1;
 }
 
-struct Circle * getCircles(){
+struct Circle * getCircles(int canvasWidth, int canvasHeight){
     printf("getCircles: entry\n");
+    // Update circle data
+    for (int i=0; i < NUM_CIRCLES; i++){
+        // Check for RIGHT collision - set x direction backwards
+        if ( (animationData[i].x + animationData[i].r) >= canvasWidth ){
+            animationData[i].xd = 0;
+        }
+        // Check for LEFT collision - set x direction forwards
+        if ( (animationData[i].x - animationData[i].r) <= 0 ){
+            animationData[i].xd = 1;
+        }
+        // Check for TOP collision - set y direction forward
+        if ( (animationData[i].y - animationData[i].r) <= 0 ){
+            animationData[i].yd = 1;
+        }
+        // Check for BOTTOM collision - set y direction backward
+        if ( (animationData[i].y + animationData[i].r) >= canvasHeight ){
+            animationData[i].yd = 0;
+        }
+
+        // Move circle in specified direction
+        if (animationData[i].xd == 1){
+            animationData[i].x += animationData[i].xv;
+        } else {
+            animationData[i].x -= animationData[i].xv;
+        }
+        if (animationData[i].yd == 1){
+            animationData[i].y += animationData[i].yv;
+        } else {
+            animationData[i].y -= animationData[i].yv;
+        }
+
+        // Update matching circle data
+        circles[i].x = animationData[i].x;
+        circles[i].y = animationData[i].y;
+
+    }
+
     printf("getCircles: exit\n");
     return circles;
 }
